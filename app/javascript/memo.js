a629da6e6852f51b1dfd3ff434690f021f14747d
Yuuki0205 <yuuki.2.5@icloud.com>
2021-04-30 18:35:54 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -3,10 +3,13 @@ const { formDisableSelector } = require("@rails/ujs");
 function post (){
   //リクエストを送信する処理
   const submit = document.getElementById("submit");
+  // console.log(submit)
   submit.addEventListener("click", (e) => {
+    console.log(e)
     e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);
+    // console.log(formData)
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
