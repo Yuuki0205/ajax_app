6ed62030fe068c3029e17bd19133b0068e879b8d
Yuuki0205 <yuuki.2.5@icloud.com>
2021-05-01 13:30:16 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,20 +1,39 @@
-const { formDisableSelector } = require("@rails/ujs");
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html
+}
 
 function post (){
-  //リクエストを送信する処理
   const submit = document.getElementById("submit");
-  // console.log(submit)
   submit.addEventListener("click", (e) => {
-    console.log(e)
     e.preventDefault();
-    const form = document.getElementById("form");
+    const form = document.getElementById("form")
     const formData = new FormData(form);
-    // console.log(formData)
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
+
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
- }
- 
- window.addEventListener('load', post);
+}
+
+window.addEventListener('load', post);
